3a2a8738ed2e301e6731b80fa2b25dc07750dd67
lopo <lopo@zju.edu.cn>
2024-08-15 18:06:52 +0800

    feat: styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
 const IndexPage = () => {
     return (
         <div>
-            <h1>hi there!</h1>
-            <p>hi there!</p>
-
-            <p>qwertyuioplkjhgfdsazxcvbnm</p>
-
-            <button>hi</button>
-
-            <a href="/">21313safdsa</a>
+            hi there!
         </div>
     )
 }
--- src/route/layout.tsx
+++ src/route/layout.tsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const RootLayout = () => {
     return (
         <>
             <div className={ 'root-banner' }>
-                <a href="/">
+                <Link style={ { marginRight: 36 } } to={ '/' }>
                     <img width={ 75 } height={ 36 } src="/lopo_animate.svg" alt=""/>
-                </a>
+                </Link>
+
+                <NavLink to={ '/poem' }>Poem</NavLink>
+                <NavLink to={ '/post' }>Post</NavLink>
+                <NavLink to={ '/project' }>Project</NavLink>
             </div>
+
             <Outlet/>
         </>
     )
