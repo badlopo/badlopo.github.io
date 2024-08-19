2390091ae3e8a66c61a1f8bec4febf82b1fddbd0
lopo <lopo@zju.edu.cn>
2024-08-19 18:13:05 +0800

    chore: archive script -- poem

---
+++ script/poem.ps1
@@ -0,0 +1,29 @@
+function main
+{
+    $public = Get-Location -PSProvider FileSystem | Join-Path -ChildPath "..\public" | Resolve-Path
+    $source = Join-Path -Path $public -ChildPath "source\poem"
+    $target = Join-Path -Path $public -ChildPath "archive\poem.json"
+    $items = Get-ChildItem -Path $source -Filter "*.md"
+
+    $sitemap = @{
+        total = $items.Count
+        date = [long]((Get-Date) - [datetime]'1970-01-01').TotalMilliseconds
+        items = New-Object System.Collections.ArrayList
+    };
+
+    foreach ($item in $items)
+    {
+        $content = Get-Content $item.FullName -Encoding UTF8
+        $line1 = $content.Split("`n")[0]
+
+        $sitemap.items.Add(@{
+            title = $line1.Substring(2)
+            path = $item.Name.Replace(".md", "")
+        }) | Out-Null
+    }
+
+    $json = ConvertTo-Json $sitemap -Depth 5 -Compress
+    Write-Output $json > $target
+}
+
+main
