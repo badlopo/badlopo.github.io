6e7af27387dbf3fdb046164e8b200a6792837dee
lopo <lopo@zju.edu.cn>
2024-08-23 00:19:38 +0800

    feat: show category & apply filter

--- src/pages/prose_gallery.tsx
+++ src/pages/prose_gallery.tsx
@@ -1,29 +1,38 @@
 import { archiveLoader, ProseArchive } from "../utils/loader.ts";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, LoaderFunctionArgs, useLoaderData, useSearchParams } from "react-router-dom";
+import { IconTag } from "../assets/icon.tsx";
 
 const ProseGalleryPage = () => {
     const { date, total, items } = useLoaderData() as ProseArchive
+    const [ searchParams, setSearchParams ] = useSearchParams()
+    const filter = searchParams.get('category')
+
+    const handleFilter = (category: string) => {
+        setSearchParams(prev => {
+            prev.set('category', category)
+            return prev
+        })
+    }
+
     return (
         <main className={ 'gallery-view prose-gallery' }>
             <h1>Prose</h1>
-
-            <br/>
-            <blockquote>I mourned for my old life, my once found paradise!</blockquote>
-            <br/>
-            <hr/>
-
             <div className={ 'meta-section' }>
                 <span>Archived: { new Date(date).toLocaleDateString() }</span>
                 <span style={ { marginLeft: 16 } }>Total: { total }</span>
             </div>
 
+            {/* TODO: show filter & clear button */ }
+
             <ul>
                 {
-                    items.map(({ title, filename }, index) => (
+                    items.map(({ filename, title, category }, index) => (
                         <li key={ index }>
-                            <Link to={ `/prose/${ filename }` }>
-                                { title }
-                            </Link>
+                            <div className={ 'category' } onClick={ () => handleFilter(category) }>
+                                <IconTag/>
+                                <span>{ category }</span>
+                            </div>
+                            <Link to={ `/prose/${ filename }` }>{ title }</Link>
                         </li>
                     ))
                 }
@@ -32,7 +41,17 @@ const ProseGalleryPage = () => {
     )
 }
 
-ProseGalleryPage.loader = () => archiveLoader('prose')
+ProseGalleryPage.loader = async ({ request }: LoaderFunctionArgs) => {
+    const proses = await archiveLoader('prose')
+
+    const url = new URL(request.url)
+    const filter = url.searchParams.get('category')
+    if(!!filter && !!proses) {
+        proses.items = proses.items.filter(prose => prose.category === filter)
+    }
+
+    return proses
+}
 
 export {
     ProseGalleryPage,
