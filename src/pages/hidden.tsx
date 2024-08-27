0af45d1897a010dfdf996bec16ab049fc7572669
lopo <lopo@zju.edu.cn>
2024-08-27 23:55:59 +0800

    chore: hidden page for test

--- src/pages/hidden.tsx
+++ src/pages/hidden.tsx
@@ -1,7 +1,18 @@
+const song = 'Ye Elders of Israel'
+
 const HiddenPage = () => {
     return (
         <main>
-            <audio src={'/audio/Ye Elders of Israel.m4a'} controls/>
+            <audio src={ '/audio/Ye Elders of Israel.m4a' } controls/>
+
+            <button onClick={() => {
+                const audio = new Audio('/audio/Ye Elders of Israel.m4a')
+                audio.play()
+            }}>play</button>
+
+            <div className={ 'player' }>
+                { song }
+            </div>
         </main>
     )
 }
