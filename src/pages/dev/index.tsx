1308a43f46123be7f61291c05a110a1939ca8ebe
lopo <lopo@zju.edu.cn>
2024-10-23 10:16:25 +0800

    example: CarouselText

--- src/pages/dev/index.tsx
+++ src/pages/dev/index.tsx
@@ -1,50 +1,115 @@
-import './index.css'
-
+import Styles from './index.module.css'
 import { useEffect, useRef } from "react";
 
-const DevPage = () => {
+type CarouselKeyframe = {
+    '--carousel-width': string,
+    '--container-bg': string,
+    '--body-offset-y': string,
+}
+
+const KEYFRAME_QUEUE: CarouselKeyframe[] = [
+    {
+        '--carousel-width': '50px', '--container-bg': '#E7F0FE',
+        '--body-offset-y': '0'
+    },
+    {
+        '--carousel-width': '100px', '--container-bg': '#E7F5EB',
+        '--body-offset-y': 'calc(-1 * var(--carousel-height))'
+    },
+    {
+        '--carousel-width': '50px', '--container-bg': '#FBEEE5',
+        '--body-offset-y': 'calc(-2 * var(--carousel-height))'
+    },
+    {
+        '--carousel-width': '50px', '--container-bg': '#E7F0FE',
+        '--body-offset-y': 'calc(-3 * var(--carousel-height))'
+    },
+    {
+        '--carousel-width': '50px', '--container-bg': '#E7F5EB',
+        '--body-offset-y': 'calc(-4 * var(--carousel-height))'
+    },
+    {
+        '--carousel-width': '80px', '--container-bg': '#FAF5D8',
+        '--body-offset-y': 'calc(-5 * var(--carousel-height))'
+    },
+    {
+        '--carousel-width': '50px', '--container-bg': '#E7F0FE',
+        '--body-offset-y': 'calc(-6 * var(--carousel-height))'
+    },
+]
+
+const CarouselText = ({ className = '' }: { className?: string }) => {
     const ref = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => {
         const el = ref.current!
 
-        el.animate([
-            { '--carousel-transform': 'translateY(0px)' },
-            { '--carousel-transform': 'translateY(30px)' },
+        let animation: Animation | null = null
+        let step = 0
 
-            // { '--carousel-offset': '24px' },
-            // { '--carousel-offset': '30px' },
+        const stepForward = () => {
+            const keyframes = KEYFRAME_QUEUE.slice(step, step + 2)
+            animation = el.animate(keyframes, {
+                // 切换动画持续时间
+                duration: 1000,
+                // 切换动画结束后停留时间
+                endDelay: 1000,
+                iterations: 1,
+                easing: 'ease',
+                fill: 'forwards'
+            })
+            animation.onfinish = () => {
+                step = (step + 1) % 6
+                stepForward()
+            }
+            animation.oncancel = () => {
+                animation = null
+                console.log('animation cancel')
+            }
+        }
+        stepForward()
 
-            // { transform: 'translateY(0)' },
-            // { transform: 'translateY(-24px)' },
-        ], {
-            duration: 1000,
-            iterations: 1,
-            easing: 'ease',
-        })
+        return () => {
+            if(animation) animation.cancel()
+        }
     }, [])
 
+    return (
+        <div ref={ ref } className={ `${ Styles.carouselContainer } ${ className }` }>
+            <div className={ Styles.carouselBody }>
+                <div className={ Styles.carouselItem } style={ { color: '#1967D2' } }>
+                    一二
+                </div>
+                <div className={ Styles.carouselItem } style={ { color: '#56A56C' } }>
+                    三四五六七
+                </div>
+                <div className={ Styles.carouselItem } style={ { color: '#D17724' } }>
+                    八九
+                </div>
+                <div className={ Styles.carouselItem } style={ { color: '#1967D2' } }>
+                    上下
+                </div>
+                <div className={ Styles.carouselItem } style={ { color: '#56A56C' } }>
+                    左右
+                </div>
+                <div className={ Styles.carouselItem } style={ { color: '#B48B00' } }>
+                    前中后
+                </div>
+                <div className={ Styles.carouselItem } style={ { color: '#1967D2' } }>
+                    一二
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const DevPage = () => {
     return (
         <main>
             <h1>Dev Page</h1>
 
             <span>prefix segment</span>
-            <div ref={ ref } className={ 'baseline carousel-container' }>
-                <div className={ 'carousel-body' }>
-                    <div className={ 'carousel-item' }>
-                        AAA
-                    </div>
-                    <div className={ 'carousel-item' }>
-                        BBB
-                    </div>
-                    <div className={ 'carousel-item' }>
-                        CCC
-                    </div>
-                    <div className={ 'carousel-item' }>
-                        BBB
-                    </div>
-                </div>
-            </div>
+            <CarouselText/>
             <span>suffix segment</span>
         </main>
     )
