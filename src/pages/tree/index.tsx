fc958ab112de2223c58a8d4eaac423105d5e54ff
lopo <lopo@zju.edu.cn>
2024-11-05 18:04:41 +0800

    feat: converter -- ProseArchive to TreeNodeLv1

--- src/pages/tree/index.tsx
+++ src/pages/tree/index.tsx
@@ -1,20 +1,56 @@
-import { archiveLoader } from "@/utils/loader.ts";
+import { archiveLoader, ProseArchive } from "@/utils/loader.ts";
+import { useEffect } from "react";
+import { TreeNodeLv1, TreeNodeLv3 } from "@/utils/tree/type.ts";
+import { useLoaderData } from "react-router-dom";
+
+type TreePageLoaderData = {
+    tree: TreeNodeLv1
+}
 
 const TreePage = () => {
-    return (
-        <main className={ 'prose-view tree' }>
+    const { tree } = useLoaderData() as TreePageLoaderData
 
-        </main>
+    useEffect(() => {
+        // TODO: render tree
+        console.log('tree:', tree)
+    }, [])
+
+    return (
+        <div className={ 'tree-container' }>
+            {/* TODO: svg element (.tree-root) */ }
+        </div>
     )
 }
 
-TreePage.loader = async () => {
+const treeBuilder = (archive: ProseArchive): TreeNodeLv1 => {
+    const categories: { [category: string]: { count: number, children: TreeNodeLv3[] } } = {}
+
+    for (const category in archive.statistics) {
+        categories[category] = {
+            count: archive.statistics[category],
+            children: [],
+        }
+    }
+    for (const item of archive.items) {
+        categories[item.category].children.push({ detail: item })
+    }
+
+    return {
+        date: archive.date,
+        children: Object.entries(categories)
+            .map(([ category, { count, children } ]) => {
+                return { category, count, children }
+            })
+    }
+}
+
+TreePage.loader = async (): Promise<TreePageLoaderData> => {
     const proses = (await archiveLoader('prose'))!
 
     // TODO: process
     console.log('got prose archive:', proses)
 
-    return null
+    return { tree: treeBuilder(proses) }
 }
 
 export {
