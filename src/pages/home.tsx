eaf9833ffd2d3904822b81329befc7292dd11501
lopo <lopo@zju.edu.cn>
2025-01-09 22:47:34 +0800

    chore: home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,19 @@
-const QUOTE = [
-    // 'Then am I',
-    // 'A happy fly,',
-    // 'If I live,',
-    // 'Or if I die.',
-    'seek out the righteous,',
-    'where’er they may be!',
-]
+// const QUOTE = [
+//     // 'Then am I',
+//     // 'A happy fly,',
+//     // 'If I live,',
+//     // 'Or if I die.',
+//     'seek out the righteous,',
+//     'where’er they may be!',
+// ]
 
 const HomePage = () => {
     return (
         <div className={ 'home-page' }>
-            <main>
-                { QUOTE.map((line, index) => <p key={ index }>{ line }</p>) }
-            </main>
+            <img style={ { width: '100%', height: '100%' } } src="/drafting.svg" alt=""/>
+            {/*<main>*/ }
+            {/*    { QUOTE.map((line, index) => <p key={ index }>{ line }</p>) }*/ }
+            {/*</main>*/ }
         </div>
     )
 }
